f818886772aded14cf3a37aa856cf7f1cc93b1da
dinosaur23 <96414278+dinosaur23@users.noreply.github.com>
2023-01-09 18:37:23 +1100

    main up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,26 +2,16 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FelixBear LIVE Docs'
+copyright = '2023, FelixBear society'
+author = 'Admin@FelixBear.WEBSERVICE'
 
 release = '0.1'
 version = '0.1.0'
 
 # -- General configuration
 
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
-
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
 
